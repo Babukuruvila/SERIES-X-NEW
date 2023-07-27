e0deca53550766da2efce5f018db8c61ed275412
Babukuruvila <bobbyjohnamion@gmail.com>
2023-07-27 12:37:50 +0530

    Fixed Spacing

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -238,7 +238,7 @@ async def next_page(bot, query):
         cap = f"<b>📁 Found ✨ Files For Your Query : #{search} 👇</b>"
     else:
         # cap = f"<b>Hᴇʏ {message.from_user.mention}, Hᴇʀᴇ ɪs ᴛʜᴇ ʀᴇsᴜʟᴛ ғᴏʀ ʏᴏᴜʀ ᴏ̨ᴜᴇʀʏ {search} \n\n</b>"
-        cap = f"<b>📁 Found ✨ Files For Your Query : #{search} 👇</b>"
+        cap = f"<b>📁 Found ✨ Files For Your Query : #{search} 👇</b>\n\n"
         for file in files:
             cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
 
@@ -1884,7 +1884,7 @@ async def auto_filter(client, msg, spoll=False):
         cap = f"<b>📁 Found ✨ Files For Your Query : #{search} 👇</b>"
     else:
         # cap = f"<b>Hᴇʏ {message.from_user.mention}, Hᴇʀᴇ ɪs ᴛʜᴇ ʀᴇsᴜʟᴛ ғᴏʀ ʏᴏᴜʀ ᴏ̨ᴜᴇʀʏ {search} \n\n</b>"
-        cap = f"<b>📁 Found ✨ Files For Your Query : #{search} 👇</b>"
+        cap = f"<b>📁 Found ✨ Files For Your Query : #{search} 👇</b>\n\n"
         for file in files:
             cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
 
