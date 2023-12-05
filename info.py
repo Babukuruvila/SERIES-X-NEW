c33211fbbec706ad8ba3207f387f9917cd7d038d
Babukuruvila <bobbyjohnamion@gmail.com>
2023-12-05 07:58:12 +0530

    id

--- info.py
+++ info.py
@@ -46,10 +46,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # FSUB
-auth_channel = environ.get('AUTH_CHANNEL', '-1001993016462')
+auth_channel = environ.get('AUTH_CHANNEL', '-1002131641137')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
-REQ_CHANNEL = environ.get("REQ_CHANNEL", "-1001993016462")
+REQ_CHANNEL = environ.get("REQ_CHANNEL", "-1002131641137")
 REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
